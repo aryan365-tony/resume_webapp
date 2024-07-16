356d6326cc913993aa8b2aa33bd6c831066f29f9
aryan365-tony <me23btech11010@iith.ac.in>
2024-07-16 17:10:59 +0530

    new commit

--- requirements.sh
+++ requirements.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env bash
+wget http://archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg-turbo/libjpeg-turbo8_2.0.3-0ubuntu1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/f/fontconfig/libfontconfig1_2.13.1-2ubuntu3_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/libx/libxext/libxext6_1.3.4-0ubuntu1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/libx/libxrender/libxrender1_0.9.10-1_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl3_3.0.2-0ubuntu1_amd64.deb
 
-# Install Python dependencies
-pip install -r requirements.txt
 
-# Download wkhtmltopdf
-wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+# Create directories for libraries
+mkdir -p lib
+mkdir -p usr/lib/x86_64-linux-gnu
 
-# Extract the executable
-dpkg -x wkhtmltox_0.12.6-1.bionic_amd64.deb wkhtmltox
+# Extract the downloaded packages
+dpkg-deb -x libjpeg-turbo8_2.0.3-0ubuntu1_amd64.deb .
+dpkg-deb -x libfontconfig1_2.13.1-2ubuntu3_amd64.deb .
+dpkg-deb -x libxext6_1.3.4-0ubuntu1_amd64.deb .
+dpkg-deb -x libxrender1_0.9.10-1_amd64.deb .
+dpkg-deb -x libssl3_3.0.2-0ubuntu1_amd64.deb .
 
-# Move the wkhtmltopdf binary to the bin directory
-mkdir -p bin
-mv wkhtmltox/usr/local/bin/wkhtmltopdf bin/
-
-# Ensure wkhtmltopdf has execute permissions
-chmod +x bin/wkhtmltopdf
+mv usr/lib/x86_64-linux-gnu/* lib/

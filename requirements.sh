d32580dab7f1224b5e8bd0164f04f3b5414cf061
aryan365-tony <me23btech11010@iith.ac.in>
2024-08-15 22:31:52 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -2,17 +2,4 @@
 
 # Install Python dependencies
 pip install -r requirements.txt
-
-# Download the static binary of wkhtmltopdf for Ubuntu Jammy (22.04)
-wget wget https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-
-# Extract the executable using ar and tar
-ar x wkhtmltox_0.12.6-1.bionic_amd64.deb
-tar -xf data.tar.xz
-
-# Create a bin directory if it doesn't exist and move the wkhtmltopdf binary to it
-mkdir -p bin
-mv usr/local/bin/wkhtmltopdf bin/
-
-# Ensure wkhtmltopdf has execute permissions
-chmod +x bin/wkhtmltopdf
+playwright install --with-deps
